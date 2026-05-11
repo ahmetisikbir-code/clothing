b424d68f5162046e5a383e48ded6411bc939059a
Clothink <clothink@clothink.com>
2026-05-11 08:00:49 +0300

    sw.js fix + ajan raporu uygulamasi

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
+self.addEventListener('install', function(e) { e.waitUntil(self.skipWaiting()); });
+self.addEventListener('activate', function(e) { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', function(e) { e.respondWith(fetch(e.request).catch(function() { return new Response('Offline', {status:503}); })); });
